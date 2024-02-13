4e93f19a565efc5b00fde522a8bc057e59521e2c
Zimil-Patel <patelzimil2310@gmail.com>
2024-02-13 22:22:31 +0530

    13-02-2024

---
+++ lib/EMI_Calculator/emi_calculator.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+class EmiCalculator extends StatefulWidget {
+  const EmiCalculator({super.key});
+
+  @override
+  State<EmiCalculator> createState() => _EmiCalculatorState();
+}
+
+double EMIAmt = 0.00;
+double loanAmt = 100000;
+double interestRate = 0;
+double tenure = 0;
+
+class _EmiCalculatorState extends State<EmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff172255),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color(0xff172255),
+          leading: const Icon(
+            Icons.window_outlined,
+            color: Colors.white,
+          ),
+          title: const Text(
+            'EMI Calculator',
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w500, color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text(
+                'Your Loan EMI is',
+                style: TextStyle(color: Colors.white, fontSize: 10),
+              ),
+              RichText(
+                  text: TextSpan(children: <TextSpan>[
+                TextSpan(
+                  text: '₹ ${EMIAmt.toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+                const TextSpan(
+                  text: '/month',
+                  style: TextStyle(color: Colors.white, fontSize: 8),
+                ),
+              ])),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(top: 20),
+                  decoration: const BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(50))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            'Loan Amount',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                          Text(
+                            loanAmt.toStringAsFixed(0),
+                            style:
+                                const TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                          Slider(
+                            value: loanAmt,
+                            min: 0,
+                            max: 200000,
+                            divisions: 100,
+                            activeColor: Colors.red,
+                            onChanged: (value) {
+                              loanAmt = value;
+                              setState(() {});
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            'Interest Rate',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                          Text(
+                            '${interestRate.toStringAsFixed(0)}%',
+                            style:
+                                const TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                          Slider(
+                            value: interestRate,
+                            min: 0,
+                            max: 100,
+                            divisions: 100,
+                            activeColor: Colors.red,
+                            onChanged: (value) {
+                              interestRate = value;
+                              setState(() {});
+                            },
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            'Loan Tenure',
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                          Text(
+                            '${tenure.toStringAsFixed(0)} months',
+                            style:
+                                const TextStyle(color: Colors.white, fontSize: 14),
+                          ),
+                          Slider(
+                            value: tenure,
+                            min: 0,
+                            max: 120,
+                            divisions: 60,
+                            activeColor: Colors.red,
+                            onChanged: (value) {
+                              tenure = value;
+                              setState(() {});
+                            },
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+
+                            EMIAmt =  calc_emi(loanAmt, interestRate, tenure);
+
+                            setState(() {
+                              
+                            });
+                          },
+                          child: const Text(
+                            'Calculate',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xff172255)),
+                          ))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+double calc_emi(double p, double r, double t)
+{
+  double emi;
+  // one month interest
+  r = r / (12 * 100);
+  // one month period
+  t = t * 12;
+  // Calculate EMI as per formula
+  emi = (p * r * pow(1 + r, t)) / (pow(1 + r, t) - 1);
+  return (emi);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:e_com_task_two/8.2/stack_app.dart';
+import 'package:e_com_task_two/EMI_Calculator/emi_calculator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 
-    initialRoute: '/stack',
+    initialRoute: '/emi',
 
     routes: {
     '/stack': (context) => const StackApp(),
+    '/emi': (context) => const EmiCalculator(),
     },
 
   ));
